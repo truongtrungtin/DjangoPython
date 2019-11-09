1ba5ae0423a63beb93dbb082c74580461060aa59
Trương Trung Tín <46252881+trungtin230599@users.noreply.github.com>
2019-11-09 21:28:59 +0700

    complete version 1.0.0

--- users/models.py
+++ users/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+#Create separate folder for each user in django and
+#  let user upload file in that folder
+def get_upload_path(instance, filename):
+    return 'profile_pics/{0}/{1}'.format(instance.user.username, filename)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
+    image = models.ImageField(default='default.jpg', upload_to=get_upload_path)
 
     def __str__(self):
         return f'{self.user.username} Profile'
