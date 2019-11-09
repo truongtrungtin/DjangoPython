69a81bbb5836f16aa9266d2f59f515ce0f742963
Trương Trung Tín <46252881+trungtin230599@users.noreply.github.com>
2019-11-09 13:12:54 +0700

    Part 9 - Update user Profile

--- trungtinblog/settings.py
+++ trungtinblog/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 #pip install django-crispy-forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
--- trungtinblog/urls.py
+++ trungtinblog/urls.py
@@ -18,6 +18,9 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
+#serving files uploaded by a user during development
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +29,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+
+
