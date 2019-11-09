7d4ef631d1fe192f23ebd9ece92d79706a4c164d
Trương Trung Tín <46252881+trungtin230599@users.noreply.github.com>
2019-11-09 20:39:53 +0700

    part 12 - Sent mail / reset password

--- trungtinblog/settings.py
+++ trungtinblog/settings.py
@@ -130,3 +130,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 LOGIN_REDIRECT_URL ='blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'a00.n00reply@gmail.com'
+EMAIL_HOST_PASSWORD  = 'tinpro123'
